43e937a376ae4940d219d013b9ddf9e141b610c8
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-04-10 16:37:24 +0900

    [Silver V] Title: 창문 닫기, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Silver/13909. 창문 닫기/창문 닫기.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include<algorithm>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	long long a;
+	cin >> a;
+
+	long long b = sqrt(a);
+	cout << b;
+
+	return 0;
+}
