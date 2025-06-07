76ad3f67ba9002d307a8c997228c81158e15ec97
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-06-07 21:50:08 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 104 ms, Memory: 8840 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,56 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+vector<vector<int>> nodes;
+vector<int> visited;
+int order = 1;
+
+void bfs(int node) {
+	queue<int> q;
+	visited[node] = order++;
+	q.push(node);
+
+	while (!q.empty()) {
+		int now = q.front();
+		q.pop();
+
+		for (auto n : nodes[now]) {
+			if (visited[n] == 0) {
+				visited[n] = order++;
+				q.push(n);
+			}
+		}
+	}
+}
+
+int main(void) {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M, R;
+	cin >> N >> M >> R;
+
+	nodes.resize(N + 1);
+	visited.resize(N + 1, 0);
+
+	int u, v;
+	while (M--) {
+		cin >> u >> v;
+		nodes[u].push_back(v);
+		nodes[v].push_back(u);
+	}
+
+	for (int i = 1; i <= N; i++)
+		sort(nodes[i].begin(), nodes[i].end(), std::greater<>());
+
+	bfs(R);
+
+	for (int i = 1; i <= N; i++)
+		cout << visited[i] << "\n";
+
+	return 0;
+}
