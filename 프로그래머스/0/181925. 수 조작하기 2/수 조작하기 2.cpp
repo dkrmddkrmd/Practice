66977219dc5a0318114641851bfcc0b9e6ff540e
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-09-03 15:49:31 +0900

    [level 0] Title: 수 조작하기 2, Time: 1.85 ms, Memory: 6.87 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.cpp
@@ -0,0 +1,30 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<int> numLog) {
+    string answer = "";
+    
+    for(int i = 1; i < numLog.size(); i++){
+        int j = numLog[i] - numLog[i-1];
+        switch(j){
+            case 1:
+                answer += "w";
+                break;
+            case -1:
+                answer += "s";
+                break;
+            case 10:
+                answer += "d";
+                break;
+            case -10:
+                answer += "a";
+                break;
+            default:
+                break;
+        }
+    }
+    
+    return answer;
+}
