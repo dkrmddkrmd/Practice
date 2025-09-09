1c7ab0202927928ecccbee299dbb9e3c4ff3fd59
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-09-09 10:03:05 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_prefix) {
+    int answer = 0;
+    int len = is_prefix.size();
+    if(my_string.substr(0, len) == is_prefix)
+        answer = 1;
+    return answer;
+}
