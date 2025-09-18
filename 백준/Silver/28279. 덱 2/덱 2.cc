cd3678d6c53b5244e5dea4723ec3343ee15e9f2c
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-09-18 14:57:26 +0900

    [Silver IV] Title: 덱 2, Time: 160 ms, Memory: 6304 KB -BaekjoonHub

--- 백준/Silver/28279. 덱 2/덱 2.cc
+++ 백준/Silver/28279. 덱 2/덱 2.cc
@@ -1,110 +1,69 @@
-#include <iostream>
-#include <deque>
+#include<iostream>
+#include<deque>
 
 using namespace std;
 
-class myDeq {
-private:
-	deque<int> d;
-
-	void one(int x) {
-		d.push_front(x);
-	}
-	void two(int x) {
-		d.push_back(x);
-	}
-	void three() {
-		if (!d.empty()) {
-			cout << d.front() << "\n";
-			d.pop_front();
-		}
-		else
-			cout << "-1\n";
-	}
-	void four() {
-		if (!d.empty()) {
-			cout << d.back() << "\n";
-			d.pop_back();
-		}
-		else
-			cout << "-1\n";
-	}
-	void five() {
-		cout << d.size() << "\n";
-	}
-	void six() {
-		if (d.empty())
-			cout << "1\n";
-		else
-			cout << "0\n";
-	}
-	void seven() {
-		if (!d.empty()) {
-			cout << d.front() << "\n";
-		}
-		else
-			cout << "-1\n";
-	}
-	
-	void eight() {
-		if (!d.empty()) {
-			cout << d.back() << "\n";
-		}
-		else
-			cout << "-1\n";
-	}
-public:
-	void order(int n) {
-		switch (n)
-		{
-		case 1: {
-			int m;
-			cin >> m;
-			one(m);
-			break;
-		}
-		case 2: {
-			int m;
-			cin >> m;
-			two(m);
-			break;
-		}
-		case 3:
-			three();
-			break;
-		case 4:
-			four();
-			break;
-		case 5:
-			five();
-			break;
-		case 6:
-			six();
-			break;
-		case 7:
-			seven();
-			break;
-		case 8:
-			eight();
-			break;
-		default:
-			break;
-		}
-	}
-
-};
-
 int main() {
-	ios_base::sync_with_stdio(false);
+	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 
-	myDeq m;
-	int N;
-	cin >> N;
-	while (N--) {
-		int n;
-		cin >> n;
-		m.order(n);
+	int order;
+	cin >> order;
+	int num;
+
+	deque<int> d;
+	while (order--) {
+		cin >> num;
+
+		switch (num) {
+		case 1:
+			cin >> num;
+			d.push_front(num);
+			break;
+		case 2:
+			cin >> num;
+			d.push_back(num);
+			break;
+		case 3:
+			if (!d.empty()) {
+				cout << d.front() << "\n";
+				d.pop_front();
+			}
+			else
+				cout << -1 << "\n";
+			break;
+		case 4:
+			if (!d.empty()) {
+				cout << d.back() << "\n";
+				d.pop_back();
+			}
+			else
+				cout << -1 << "\n";
+			break;
+		case 5:
+			cout << d.size() << "\n";
+			break;
+		case 6:
+			if (d.empty())
+				cout << 1 << "\n";
+			else
+				cout << 0 << "\n";
+			break;
+		case 7:
+			if (!d.empty()) {
+				cout << d.front() << "\n";
+			}
+			else
+				cout << -1 << "\n";
+			break;
+		case 8:
+			if (!d.empty()) {
+				cout << d.back() << "\n";
+			}
+			else
+				cout << -1 << "\n";
+			break;
+		}
 	}
 
 	return 0;
