65597837b98a2fe4a70ef38b1832e4378405b981
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-09-30 21:43:33 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.19 ms, Memory: 74.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = {};
+        
+        answer = my_string.split(" ");
+        
+        return answer;
+    }
+}
