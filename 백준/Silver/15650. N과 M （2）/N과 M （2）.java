228d0dab8c244bcbf8796f398c7ea9333bd09b46
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-10-13 22:48:11 +0900

    [Silver III] Title: N과 M (2), Time: 104 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static Deque<Integer> deque = new ArrayDeque<>();
+    static StringBuilder stringBuilder = new StringBuilder();
+    static boolean[] arr;
+
+    static int N, M;
+
+    static void backTracking(int n, int start, int M){
+        if(n == M){
+            for(int d : deque){
+                stringBuilder.append(d).append(" ");
+            }
+            stringBuilder.append("\n");
+            return;
+        }
+
+        for(int i = start; i <= N; i++){
+            if(!arr[i]){
+                deque.offerLast(i);
+                arr[i] = true;
+                backTracking(n+1, i + 1, M);
+                deque.pollLast();
+                arr[i] = false;
+            }
+        }
+    }
+
+    public static void  main(String[] args) throws  Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new boolean[N + 1];
+
+        backTracking(0, 1, M);
+
+        System.out.println(stringBuilder);
+    }
+}
