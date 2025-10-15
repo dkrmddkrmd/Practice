369b7aebf5658260f0306a0c0286b1faa3ef25d5
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-10-16 08:53:39 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 924 ms, Memory: 89248 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.java
@@ -0,0 +1,72 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+    static int N, M, R;
+
+    static List<List<Integer>> graph;
+
+    static boolean[] visited;
+    static int[] visit;
+
+    static StringBuilder ans;
+    static int seq = 1;
+
+    static void bfs(int start){
+        visited[start] = true;
+        Deque<Integer> deque = new ArrayDeque<>();
+        deque.offerLast(start);
+        visit[start] = seq++;
+
+        while (!deque.isEmpty()){
+            int now = deque.pollFirst();
+            for (int vec : graph.get(now)){
+                if(!visited[vec]){
+                    visited[vec] = true;
+                    deque.offerLast(vec);
+                    visit[vec] = seq++;
+                }
+            }
+        }
+    }
+
+
+    public static void  main(String[] args) throws  Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        for(int i = 0; i <= N; i++){
+            graph.add(new ArrayList<>());
+        }
+        ans = new StringBuilder();
+        visited = new boolean[N+1];
+        visit = new int[N+1];
+
+        for(int i = 0; i < M; i++){
+            st = new StringTokenizer(br.readLine());
+
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        for(int i = 1; i <= N; i++){
+            Collections.sort(graph.get(i), Collections.reverseOrder());
+        }
+
+        bfs(R);
+
+        for(int i = 1; i <= N; i++){
+            ans.append(visit[i]).append("\n");
+        }
+
+        System.out.println(ans);
+    }
+}
