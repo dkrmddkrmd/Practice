cda0e87e5c8e543d0d098fbb9a037c6f3f27963f
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-11-18 23:35:57 +0900

    [Bronze III] Title: 알람 시계, Time: 124 ms, Memory: 16084 KB -BaekjoonHub

--- 백준/Bronze/2884. 알람 시계/알람 시계.java
+++ 백준/Bronze/2884. 알람 시계/알람 시계.java
@@ -6,18 +6,16 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int H = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
         M -= 45;
         if(M < 0){
-            M = 60 + M;
-            H--;
-            if(H < 0)
-                H = 24 + H;
+            M += 60;
+            N -= 1;
+            if(N < 0)
+                N += 24;
         }
-
-        System.out.println(H + " " + M);
+        System.out.println(N + " " + M);
     }
 }
-
