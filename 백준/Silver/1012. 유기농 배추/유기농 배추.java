258f700ce0d46b7cd3c2d3c7816337d63b3ddd62
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-11-20 21:04:57 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 15936 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,84 +1,77 @@
 import java.util.*;
 import java.io.*;
 
-class Ground{
-    int row;
-    int col;
+class Node{
+    int x;
+    int y;
 
-    public Ground(int row, int col) {
-        this.row = row;
-        this.col = col;
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
 }
 
 public class Main{
-    static int[][] ground;
-
-    static int M;
-    static int N;
+    static int M, N, K;
+    static int earthworm = 0;
 
     // 상 우 하 좌
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
+    static int[] dx = {0, 1, 0, -1};
+    static int[] dy = {-1, 0, 1, 0};
 
-    static int worm = 0;
-    static StringBuilder sb = new StringBuilder();
+    static void dfs(int[][] ground, int y, int x){
+        Deque<Node> nodes = new ArrayDeque<>();
+        earthworm++;
+        nodes.offerLast(new Node(x, y));
 
-    static void bfs(int row, int col){
-        Deque<Ground> dq = new ArrayDeque<>();
-        dq.offerLast(new Ground(row, col));
-
-        while (!dq.isEmpty()){
-            Ground now = dq.pollFirst();
+        while (!nodes.isEmpty()){
+            Node now = nodes.pollFirst();
             for(int i = 0; i < 4; i++){
-                int nextRow = now.row + dr[i];
-                int nextCol = now.col + dc[i];
+                int nextX = now.x + dx[i];
+                int nextY = now.y + dy[i];
 
-                if(nextRow >= 0 && nextRow < N && nextCol >= 0 && nextCol < M && ground[nextRow][nextCol] == 1){
-                    ground[nextRow][nextCol] = 2;
-                    dq.offerLast(new Ground(nextRow, nextCol));
+                if(check(ground, nextY, nextX)){
+                    nodes.offerLast(new Node(nextX, nextY));
+                    ground[nextY][nextX] = 2;
                 }
             }
         }
     }
 
+    static boolean check(int[][] ground, int y, int x){
+        return y >= 0 && y < N && x >= 0 && x < M && ground[y][x] == 1;
+    }
+
     public static void  main(String[] args) throws  Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int T = Integer.parseInt(st.nextToken());
-
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
         while (T-- > 0){
-            st = new StringTokenizer(br.readLine());
-
-            // 가로길이
+            StringTokenizer st = new StringTokenizer(br.readLine());
             M = Integer.parseInt(st.nextToken());
-
-            // 세로 길이
             N = Integer.parseInt(st.nextToken());
-            // 배추 개수
-            int K = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            earthworm = 0;
 
-            ground = new int[N][M];
+            int[][] ground = new int[N][M];
 
             for(int i = 0; i < K; i++){
                 st = new StringTokenizer(br.readLine());
                 int X = Integer.parseInt(st.nextToken());
                 int Y = Integer.parseInt(st.nextToken());
+
                 ground[Y][X] = 1;
             }
 
             for(int i = 0; i < N; i++){
                 for(int j = 0; j < M; j++){
                     if(ground[i][j] == 1){
-                        bfs(i, j);
-                        worm++;
+                        dfs(ground, i, j);
                     }
                 }
             }
 
-            sb.append(worm).append("\n");
-            worm = 0;
+            sb.append(earthworm).append("\n");
         }
 
         System.out.println(sb);
