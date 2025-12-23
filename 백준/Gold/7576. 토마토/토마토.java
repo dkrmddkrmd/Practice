2ab741ff34985620b2ff290f9d3f4f3d00d8cb3c
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2025-12-23 17:52:01 +0900

    [Gold V] Title: 토마토, Time: 564 ms, Memory: 102848 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -6,7 +6,7 @@ class Tomato{
     int col;
     int day;
 
-    public Tomato(int row, int col, int day) {
+    public Tomato(int row, int col, int day){
         this.row = row;
         this.col = col;
         this.day = day;
@@ -14,40 +14,13 @@ class Tomato{
 }
 
 public class Main{
-    static int badTomatos;
-    static int[][] ground;
-    static Deque<Tomato> goodTomatos;
-    static int N, M;
-    static int day = 0;
+    static int M, N;
+    static int[][] tomato;
+    static Deque<Tomato> tomatoes;
 
-    //상 우 하 좌
-    static int[] dr = {-1, 0, 1, 0};
-    static int[] dc = {0, 1, 0, -1};
-
-    static void dfs(){
-        while (!goodTomatos.isEmpty()){
-            Tomato now = goodTomatos.pollFirst();
-
-            for(int i = 0; i < 4; i++){
-                int nextRow = now.row + dr[i];
-                int nextCol = now.col + dc[i];
-                int nextDay = now.day + 1;
-
-                if(check(nextRow, nextCol)){
-                    if(ground[nextRow][nextCol] == 0){
-                        ground[nextRow][nextCol] = 1;
-                        goodTomatos.offerLast(new Tomato(nextRow, nextCol, nextDay));
-                        badTomatos--;
-                        day = Math.max(day, nextDay);
-                    }
-                }
-            }
-        }
-    }
-
-    static boolean check(int row, int col){
-        return row >= 0 && row < N && col >= 0 && col < M;
-    }
+    //우 하 좌 상
+    static int[] dr = {0, 1, 0, -1};
+    static int[] dc = {1, 0, -1, 0};
 
     public static void  main(String[] args) throws  Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -56,34 +29,71 @@ public class Main{
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
 
-        ground = new int[N][M];
-        badTomatos = 0;
-        goodTomatos = new ArrayDeque<>();
+        tomato = new int[N][M];
+        tomatoes = new ArrayDeque<>();
+
+        boolean isNotTomato = true;
 
         for(int i = 0; i < N; i++){
             st = new StringTokenizer(br.readLine());
             for(int j = 0; j < M; j++){
-                int tomato = Integer.parseInt(st.nextToken());
-                ground[i][j] = tomato;
-                if(tomato == 0)
-                    badTomatos++;
-                else if(tomato == 1){
-                    goodTomatos.offerLast(new Tomato(i, j, 0));
+                int num = Integer.parseInt(st.nextToken());
+                tomato[i][j] = num;
+                if(num == 1){
+                    tomatoes.offerLast(new Tomato(i, j, 0));
+                }
+                else if(num == 0 && isNotTomato){
+                    isNotTomato = false;
                 }
             }
         }
 
-        if(badTomatos == 0){
+        if(isNotTomato){
             System.out.println(0);
             return;
         }
 
         dfs();
 
-        if(badTomatos == 0)
-            System.out.println(day);
-        else{
+        boolean notTomato = false;
+        int day = 0;
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < M; j++){
+                if(tomato[i][j] == 0){
+                    notTomato = true;
+                    break;
+                }
+                day = Integer.max(day, tomato[i][j]);
+            }
+        }
+
+        if(notTomato){
             System.out.println(-1);
         }
+        else{
+            System.out.println(day);
+        }
+    }
+
+    static void dfs(){
+        while (!tomatoes.isEmpty()){
+            Tomato nowTomato = tomatoes.pollFirst();
+
+            for(int i = 0; i < 4; i++){
+                int nextRow = nowTomato.row + dr[i];
+                int nextCol = nowTomato.col + dc[i];
+
+                if(!isTomato(nextRow, nextCol) || tomato[nextRow][nextCol] != 0)
+                    continue;
+
+                tomato[nextRow][nextCol] = nowTomato.day+1;
+                tomatoes.offerLast(new Tomato(nextRow, nextCol, nowTomato.day+1));
+            }
+        }
+    }
+
+    static boolean isTomato(int row, int col){
+        return (row >= 0 && row < N && col >= 0 && col < M);
     }
 }
