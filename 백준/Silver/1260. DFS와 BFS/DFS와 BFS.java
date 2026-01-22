a7f2ae0fbbeec3a9fa4889ba56720ff6d7a8e95d
dkrmddkrmd <122345039+dkrmddkrmd@users.noreply.github.com>
2026-01-22 11:49:49 +0900

    [Silver II] Title: DFS와 BFS, Time: 184 ms, Memory: 19148 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,94 +1,88 @@
+import java.lang.reflect.Array;
 import java.util.*;
 import java.io.*;
 
 public class Main{
     static int N, M, V;
-
-    static List<List<Integer>> graph;
-    static boolean[] visited1;
-    static int[] visit1;
-    static int seq1 = 1;
-
-    static StringBuilder sb1;
-    static StringBuilder sb2;
-
-    static void dfs(int start){
-        visited1[start] = true;
-        visit1[seq1++] = start;
-
-        for(int next : graph.get(start)){
-            if(!visited1[next]){
-                dfs(next);
-            }
-        }
-    }
-
-    static void bfs(int start){
-        visited1[start] = true;
-        visit1[seq1++] = start;
-
-        Deque<Integer> deque = new ArrayDeque<>();
-        deque.offerLast(start);
-
-        while (!deque.isEmpty()){
-            int now = deque.pollFirst();
-            for (int next : graph.get(now)){
-                if(!visited1[next]){
-                    visited1[next] = true;
-                    visit1[seq1++] = next;
-                    deque.offerLast(next);
-                }
-            }
-        }
-    }
+    static ArrayList<Integer>[] arr;
+    static ArrayList<Integer> arrD, arrB;
+    static boolean[] checkD, checkB;
 
     public static void  main(String[] args) throws  Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        int V = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
 
-        visited1 = new boolean[N+1];
-        visit1 = new int[N+1];
-        graph = new ArrayList<>();
-        for(int i = 0; i <= N; i++){
-            graph.add(new ArrayList<>());
+        arr = new ArrayList[N+1];
+        for(int i = 1; i <= N; i++){
+            arr[i] = new ArrayList<>();
         }
-        sb1 = new StringBuilder();
-        sb2 = new StringBuilder();
+
+        arrD = new ArrayList<>();
+        arrB = new ArrayList<>();
+        checkB = new boolean[N+1];
+        checkD = new boolean[N+1];
 
         for(int i = 0; i < M; i++){
             st = new StringTokenizer(br.readLine());
+            int n1 = Integer.parseInt(st.nextToken());
+            int n2 = Integer.parseInt(st.nextToken());
 
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+            arr[n1].add(n2);
+            arr[n2].add(n1);
         }
 
-        for (int i = 1; i <= N; i++){
-            Collections.sort(graph.get(i));
+        for(int i = 1; i <= N; i++){
+            Collections.sort(arr[i]);
         }
 
         dfs(V);
-        for (int i =1; i <= N; i++){
-            if(visit1[i] != 0)
-                sb1.append(visit1[i]).append(" ");
-        }
-        Arrays.fill(visited1, false);
-        seq1 = 1;
-        sb1.append("\n");
+
         bfs(V);
-        for (int i =1; i <= N; i++){
-            if(visit1[i] != 0)
-                sb1.append(visit1[i]).append(" ");
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int num : arrD){
+            sb.append(num).append(" ");
         }
 
+        sb.append("\n");
 
-        System.out.println(sb1);
+        for(int num : arrB){
+            sb.append(num).append(" ");
+        }
 
+        System.out.println(sb);
+    }
+
+    static void dfs(int V){
+        checkD[V] = true;
+        arrD.add(V);
+
+        for(int num : arr[V]){
+            if(!checkD[num])
+                dfs(num);
+        }
+    }
+
+    static void bfs(int V){
+        Deque<Integer> dq = new ArrayDeque<>();
+        dq.offerLast(V);
+        checkB[V] = true;
+
+        while (!dq.isEmpty()){
+            int now = dq.pollFirst();
+            arrB.add(now);
+
+            for(int num : arr[now]){
+                if(!checkB[num]){
+                    dq.offerLast(num);
+                    checkB[num] = true;
+                }
+            }
+        }
     }
 }
