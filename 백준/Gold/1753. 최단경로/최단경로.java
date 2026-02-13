87b45b290be6b62c86f98c9f6e69f49674861447
Bohyun Kim <122345039+dkrmddkrmd@users.noreply.github.com>
2026-02-13 15:24:26 +0900

    [Gold IV] Title: 최단경로, Time: 524 ms, Memory: 109008 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -2,73 +2,74 @@ import java.util.*;
 import java.io.*;
 
 class Node implements Comparable<Node>{
-    int index;
-    int cost;
+    int to;
+    int weight;
 
-    public Node(int index, int cost){
-        this.index = index;
-        this.cost = cost;
+    public Node(int to, int weight) {
+        this.to = to;
+        this.weight = weight;
     }
 
-    public int compareTo(Node node){
-        return this.cost - node.cost;
+    public int compareTo(Node o){
+        return this.weight - o.weight;
     }
 }
 
-public class Main{
-    static int V, E, K;
+public class Main {
+    static ArrayList<Node>[] nodes;
+    static int[] dist;
 
-    public static void  main(String[] args) throws  Exception{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        V = Integer.parseInt(st.nextToken());
-        E = Integer.parseInt(st.nextToken());
-        K = Integer.parseInt(br.readLine());
+        int V = Integer.parseInt(st.nextToken());
+        int E = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        int dist[] = new int[V+1];
-        ArrayList<Node>[] adj = new ArrayList[V+1];
-
-        Arrays.fill(dist, Integer.MAX_VALUE);
-        dist[K] = 0;
-
+        nodes = new ArrayList[V+1];
         for(int i = 1; i <= V; i++){
-            adj[i] = new ArrayList<>();
+            nodes[i] = new ArrayList<>();
         }
+
+        dist = new int[V+1];
+        Arrays.fill(dist, Integer.MAX_VALUE);
+
+        int root = Integer.parseInt(br.readLine());
+        dist[root] = 0;
 
         for(int i = 0; i < E; i++){
             st = new StringTokenizer(br.readLine());
+
             int u = Integer.parseInt(st.nextToken());
             int v = Integer.parseInt(st.nextToken());
-            int e = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
 
-            adj[u].add(new Node(v, e));
+            nodes[u].add(new Node(v, w));
         }
 
-        pq.offer(new Node(K, 0));
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(root, 0));
+
         while (!pq.isEmpty()){
             Node now = pq.poll();
 
-            if(dist[now.index] < now.cost)
-                continue;
+            for(Node node : nodes[now.to]){
+                int nextDist = now.weight + node.weight;
 
-            for (Node node : adj[now.index]){
-                if(dist[node.index] > dist[now.index] + node.cost){
-                    dist[node.index] = dist[now.index] + node.cost;
-                    pq.offer(new Node(node.index, dist[node.index]));
-                }
+                if(nextDist >= dist[node.to])
+                    continue;
+
+                dist[node.to] = nextDist;
+                pq.offer(new Node(node.to, nextDist));
             }
         }
 
         StringBuilder sb = new StringBuilder();
         for(int i = 1; i <= V; i++){
-            if(dist[i] == Integer.MAX_VALUE){
+            if(dist[i] == Integer.MAX_VALUE)
                 sb.append("INF\n");
-            }
-            else{
+            else
                 sb.append(dist[i]).append("\n");
-            }
         }
 
         System.out.println(sb);
