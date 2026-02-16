c3a04b65f39176261270105fd6ca4e2cd760e70f
Bohyun Kim <122345039+dkrmddkrmd@users.noreply.github.com>
2026-02-16 09:41:06 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 23064 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,42 +1,43 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    static Deque<Integer> deque = new ArrayDeque<>();
-    static StringBuilder stringBuilder = new StringBuilder();
-    static boolean[] arr;
-
+public class Main {
     static int N, M;
+    static boolean[] visited;
+    static int[] arr;
+    static StringBuilder sb = new StringBuilder();
 
-    static void backTracking(int n, int M){
-        if(n == M){
-            for(int d : deque){
-                stringBuilder.append(d).append(" ");
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[N+1];
+        arr = new int[M];
+
+        backTracking(0);
+
+        System.out.println(sb);
+    }
+
+    static void backTracking(int depth){
+        if(depth == M){
+            for(int i = 0; i < depth; i++){
+                sb.append(arr[i]).append(" ");
             }
-            stringBuilder.append("\n");
+            sb.append("\n");
             return;
         }
 
         for(int i = 1; i <= N; i++){
-            if(!arr[i]){
-                deque.offerLast(i);
-                arr[i] = true;
-                backTracking(n+1, M);
-                deque.pollLast();
-                arr[i] = false;
+            if(!visited[i]){
+                visited[i] = true;
+                arr[depth] = i;
+                backTracking(depth+1);
+                visited[i] = false;
             }
         }
-    }
-
-    public static void  main(String[] args) throws  Exception{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new boolean[N + 1];
-
-        backTracking(0, M);
-
-        System.out.println(stringBuilder);
     }
 }
