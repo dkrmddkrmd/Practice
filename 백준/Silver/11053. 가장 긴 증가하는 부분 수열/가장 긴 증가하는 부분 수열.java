efc274cc577c57b7c9c1c066ccb8e1dc4b7a1cf7
Bohyun Kim <122345039+dkrmddkrmd@users.noreply.github.com>
2026-02-19 13:51:28 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,31 +1,45 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    public static void  main(String[] args) throws  Exception{
+public class Main {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int[] arr = new int[N];
-        int[] dp = new int[N];
 
         st = new StringTokenizer(br.readLine());
-        int maxDp = 0;
-        for(int i = 0; i < N; i++){
+
+        int[] arr = new int[N];
+
+        arr[0] = Integer.parseInt(st.nextToken());
+        int len = 1;
+
+        for(int i = 1; i < N; i++){
             int num = Integer.parseInt(st.nextToken());
 
-            arr[i] = num;
-
-            dp[i] = 1;
-            for(int j = 0; j < i; j++){
-                if(arr[j] < arr[i]){
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+            if(num > arr[len-1]){
+                arr[len] = num;
+                len++;
+                continue;
             }
-            maxDp = Math.max(maxDp, dp[i]);
+
+            // 값이 가장 작으면
+            if(arr[0] > num){
+                arr[0] = num;
+                continue;
+            }
+
+            for(int j = len - 1; j >= 0; j--){
+                if(arr[j] >= num)
+                    continue;
+
+                // 더 작은 값을 만나면
+                arr[j+1] = num;
+                break;
+            }
         }
 
-        System.out.println(maxDp);
+        System.out.println(len);
     }
 }
