fd3d0aa049145f9c598f87459861eea710fae518
Bohyun Kim <122345039+dkrmddkrmd@users.noreply.github.com>
2026-03-27 20:06:54 +0900

    [Gold IV] Title: 최단경로, Time: 644 ms, Memory: 107960 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -16,26 +16,27 @@ class Node implements Comparable<Node>{
 }
 
 public class Main {
-    static ArrayList<Node>[] nodes;
+    static int V, E, K;
+    static List<Node>[] nodes;
     static int[] dist;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int V = Integer.parseInt(st.nextToken());
-        int E = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        E = Integer.parseInt(st.nextToken());
+
+        K = Integer.parseInt(br.readLine());
 
         nodes = new ArrayList[V+1];
         for(int i = 1; i <= V; i++){
             nodes[i] = new ArrayList<>();
         }
-
         dist = new int[V+1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
 
-        int root = Integer.parseInt(br.readLine());
-        dist[root] = 0;
+        Arrays.fill(dist, Integer.MAX_VALUE);
+        dist[K] = 0;
 
         for(int i = 0; i < E; i++){
             st = new StringTokenizer(br.readLine());
@@ -47,24 +48,28 @@ public class Main {
             nodes[u].add(new Node(v, w));
         }
 
+        dijkstra(K);
+    }
+
+    static void dijkstra(int start){
         PriorityQueue<Node> pq = new PriorityQueue<>();
-        pq.offer(new Node(root, 0));
+        pq.offer(new Node(K, 0));
 
         while (!pq.isEmpty()){
             Node now = pq.poll();
+            if(dist[now.to] < now.weight)
+                continue;
 
-            for(Node node : nodes[now.to]){
-                int nextDist = now.weight + node.weight;
-
-                if(nextDist >= dist[node.to])
-                    continue;
-
-                dist[node.to] = nextDist;
-                pq.offer(new Node(node.to, nextDist));
+            for(Node next : nodes[now.to]){
+                if(dist[now.to] + next.weight < dist[next.to]){
+                    dist[next.to] = dist[now.to] + next.weight;
+                    pq.offer(new Node(next.to, dist[next.to]));
+                }
             }
         }
 
         StringBuilder sb = new StringBuilder();
+
         for(int i = 1; i <= V; i++){
             if(dist[i] == Integer.MAX_VALUE)
                 sb.append("INF\n");
