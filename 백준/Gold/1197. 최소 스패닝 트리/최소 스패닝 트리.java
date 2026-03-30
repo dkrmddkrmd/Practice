3e101ea3559a457269f3720e8e467433d35fce8a
Bohyun Kim <122345039+dkrmddkrmd@users.noreply.github.com>
2026-03-30 11:19:28 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 464 ms, Memory: 49408 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,9 +1,27 @@
 import java.util.*;
 import java.io.*;
 
+class Node implements Comparable<Node>{
+    int from;
+    int to;
+    int weight;
+
+    public Node(int from, int to, int weight) {
+        this.from = from;
+        this.to = to;
+        this.weight = weight;
+    }
+
+    public int compareTo(Node o){
+        return this.weight - o.weight;
+    }
+}
+
 public class Main {
+    static int V, E;
+    static PriorityQueue<Node> pq;
     static int[] parent;
-    static ArrayList<Vertex> adj;
+    static long dist;
 
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,12 +30,13 @@ public class Main {
         int V = Integer.parseInt(st.nextToken());
         int E = Integer.parseInt(st.nextToken());
 
+
         parent = new int[V+1];
+        pq = new PriorityQueue<>();
+
         for(int i = 1; i <= V; i++){
             parent[i] = i;
         }
-
-        adj = new ArrayList<>();
 
         for(int i = 0; i < E; i++){
             st = new StringTokenizer(br.readLine());
@@ -25,57 +44,37 @@ public class Main {
             int A = Integer.parseInt(st.nextToken());
             int B = Integer.parseInt(st.nextToken());
             int C = Integer.parseInt(st.nextToken());
-
-            adj.add(new Vertex(A, B, C));
+            pq.offer(new Node(A, B, C));
         }
 
-        Collections.sort(adj);
+        int isTree = 0;
+        while (!pq.isEmpty() && isTree != V - 1){
+            Node now = pq.poll();
 
-        int connection = 0;
-        int ans = 0;
+            int rootA = find(now.from);
+            int rootB = find(now.to);
 
-        for(int i = 0; i < E && (connection != V - 1); i++){
-            Vertex v = adj.get(i);
-
-            int a = v.from;
-            int b = v.to;
-
-            if(find(a) != find(b)){
-                union(a, b);
-                connection++;
-                ans += v.weight;
+            if(rootA != rootB){
+                union(now.from, now.to);
+                dist += now.weight;
+                isTree++;
             }
         }
 
-        System.out.println(ans);
+        System.out.println(dist);
     }
-
-    static class Vertex implements Comparable<Vertex>{
-        int from;
-        int to;
-        int weight;
-
-        public Vertex(int from, int to, int weight) {
-            this.from = from;
-            this.to = to;
-            this.weight = weight;
-        }
-
-        public int compareTo(Vertex o){
-            return this.weight - o.weight;
-        }
-    }
-
     static void union(int x, int y){
         int rootX = find(x);
         int rootY = find(y);
 
-        if(rootX != rootY)
-            parent[rootY] = rootX;
+        if(rootX == rootY)
+            return;
+
+        parent[rootY] = rootX;
     }
 
     static int find(int x){
-        if(x == parent[x])
+        if(parent[x] == x)
             return x;
 
         return parent[x] = find(parent[x]);
